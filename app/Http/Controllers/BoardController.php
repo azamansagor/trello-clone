ecf8e7a3dfa9e717f07487a3d5c4dfa40c60b5cb
A Zaman Sagor <a.zamansagor@gmail.com>
2020-03-22 01:31:54 +0600

    First commit

---
+++ app/Http/Controllers/BoardController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Board;
+
+class BoardController extends Controller{
+
+/**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        
+    }
+
+    public function index(){
+    	return Board::all();
+    }
+
+    public function show($id){
+    	$board = Board::findOrFail($id);
+    	return $board;
+    }
+
+    public function store(Request $request){
+
+    	Board::create([
+    		'name' 		=> $request->name,
+    		'user_id'	=> 1
+    	]);
+
+    	return response()->json([ 'message' => 'success'], 200);
+    }
+
+}
